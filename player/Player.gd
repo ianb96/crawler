a62beca5b0ec105b1a3be2256cef94f34801c1d6
ianb96 <ianb96@gmail.com>
2017-05-09 18:27:31 -0400

    movement anim integrated

--- player/Player.gd
+++ player/Player.gd
@@ -6,10 +6,12 @@ const ACCELERATION = 2
 
 var mouse_mode = false
 
-var curHealth
+var cur_health
 var vel
 var facing = "down"
-#var bullet = preload("bullet.tscn")
+var anim_state = ""
+var bullet = preload("res://bullet.tscn")
+onready var gunpos = get_node("sprite/gunpos").get_pos()
 
 func _ready():
 	set_fixed_process(true)
@@ -31,7 +33,7 @@ func _fixed_process(delta):
 		facing = "right"
 		moving = true
 	elif invec.x<0:
-		facing = "right"
+		facing = "left"
 		moving = true
 	elif invec.y>0:
 		facing = "down"
@@ -39,10 +41,15 @@ func _fixed_process(delta):
 	elif invec.y<0:
 		facing = "up"
 		moving = true
+	var new_anim_state
 	if moving:
-		var anim_state="run_"+facing
+		new_anim_state="run_"+facing
 	else:
-		var anim_state="idle_"+facing
+		new_anim_state="idle_"+facing
+	if new_anim_state != anim_state:
+		#best way?
+		get_node("anim").play(new_anim_state)
+		anim_state = new_anim_state
 	# move
 	vel = invec.normalized() * MAX_SPEED * delta
 	var rem = move(vel)
@@ -50,11 +57,15 @@ func _fixed_process(delta):
 	if (is_colliding()):
 		var n = get_collision_normal()
 		var motion = n.slide(rem)
+		# slide velocity?
 		move(motion)
 	
 	# attack
 	if Input.is_action_pressed("fire"):
-#		bullet.instance()
-		pass
+		var b = bullet.instance()
+		b.set_pos(gunpos)
 
-
+func takeDamage(var amount):
+	cur_health-=amount
+	if cur_health<=0:
+		print("dead")
